b66f4cbe35854733d2f41d1b34451461568474f5
MICHAELABICK <MichaelBick@Michaels-MacBook-Pro-7.local>
2019-08-28 21:29:44 -0400

    Added current code sitting on my laptop

--- test2.m
+++ test2.m
@@ -2,25 +2,29 @@ clc
 
 close
 
-motor = createMotor(18700, 0.7, 0.71, 134);
+pro = createMotor(18700, 0.7, 0.71, 134);
+cim = createMotor(5310, 2.7, 2.41, 131);
 
 time = 3;
 vi = 0;
 
+
+motor1 = cim;
 dia1 = 4 * .0254;
-ratio1 = 16;
+ratio1 = 5;
 numMotors1 = 4;
-currLim1 = 1500;
+currLim1 = 300;
 mass1 = 60;
 
+motor2 = pro;
 dia2 = 4 * .0254;
-ratio2 = 12;
-numMotors2 = 8;
-currLim2 = 500;
+ratio2 = 5 * 18700 / 5310;
+numMotors2 = 4;
+currLim2 = 300;
 mass2 = 60;
 
-[t1, x1, v1, a1] = lineSim(time, motor, numMotors1, currLim1, mass1, 1.1, vi, dia1, ratio1);
-[t2, x2, v2, a2] = lineSim(time, motor, numMotors2, currLim2, mass2, 1.1, vi, dia2, ratio2);
+[t1, x1, v1, a1] = lineSim(time, motor1, numMotors1, currLim1, mass1, 1.1, vi, dia1, ratio1);
+[t2, x2, v2, a2] = lineSim(time, motor2, numMotors2, currLim2, mass2, 1.1, vi, dia2, ratio2);
 
 % Convert to feet
 x1 = x1 .* 3.28;
